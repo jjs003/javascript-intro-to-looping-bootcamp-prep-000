b95be5e1d6f866b1f454ae63cb1ee910630f4770
Jeremy Snow <jeremysnow404@yahoo.com>
2017-04-21 03:05:33 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,11 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++){
+    if (i === 1) {
+      array[i-1] = "I am 1 strange loop"
+    }
+
+    else {
+      array[i-1] = `I am ${i} strange loops.`
+    }
+  }
+}
