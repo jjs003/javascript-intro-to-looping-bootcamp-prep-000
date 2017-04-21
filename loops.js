fe357fcaebedfcc0d5887cb968ef3054a275fa1f
Jeremy Snow <jeremysnow404@yahoo.com>
2017-04-21 03:26:34 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,11 +1,32 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++){
     if (i === 1) {
-      array[i-1] = "I am 1 strange loop"
+      array.push("I am 1 strange loop")
     }
 
     else {
-      array[i-1] = `I am ${i} strange loops.`
+      array.push(`I am ${i} strange loops.`)
     }
   }
+
+  return array
+}
+
+function whileLoop(num) {
+  while (num > 0) {
+    console.log(num--)
+  }
+  return 'done'
+}
+
+function doWhileLoop (array) {
+
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+do {
+  array.pop()
+}while (array.length > 0 && maybeTrue());
+return array
 }
